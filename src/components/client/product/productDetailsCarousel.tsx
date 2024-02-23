fab16cb75c39126e4c6f873eedc226cb286d5885
Abdul-Manan-456 <abdulmanan7733@gmail.com>
2024-02-23 11:38:36 +0500

    changes

--- src/components/client/product/productDetailsCarousel.tsx
+++ src/components/client/product/productDetailsCarousel.tsx
@@ -31,6 +31,7 @@ const ProductDetailsCarousel = () => {
         },
         [api]
     );
+
     useEffect(() => {
         if (!api) {
             return;
--- src/lib/config/apiConfig.ts
+++ src/lib/config/apiConfig.ts
@@ -1,3 +1,6 @@
-// export const baseUrl: string = "http://localhost:3500/api/v1";
-export const baseUrl: string = "https://ecommerce-backend-j8v696v1w-abdul-manan-456s-projects.vercel.app/api/v1";
+
+// const
+export const baseUrl: string = "http://localhost:3500/api/v1";
+
+// export const baseUrl: string = "https://ecommerce-backend-j8v696v1w-abdul-manan-456s-projects.vercel.app/api/v1";
 // 
